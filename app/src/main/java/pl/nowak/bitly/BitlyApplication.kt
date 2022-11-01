3cefd48ee178ef955ea40676a2de45f0479c630f
Bartosz Nowak <9051964+DuMaM@users.noreply.github.com>
2022-11-01 19:31:04 +0100

    feat: improve timber logging

--- app/src/main/java/pl/nowak/bitly/BitlyApplication.kt
+++ app/src/main/java/pl/nowak/bitly/BitlyApplication.kt
@@ -6,6 +6,29 @@ import timber.log.Timber
 class BitlyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree())
+        Timber.plant(object : Timber.DebugTree() {
+            /**
+             * Override [log] to modify the tag and add a "global tag" prefix to it. You can rename the String "global_tag_" as you see fit.
+             */
+            override fun log(
+                priority: Int, tag: String?, message: String, t: Throwable?
+            ) {
+                super.log(priority, "[BL] $tag", message, t)
+            }
+
+
+            /**
+             * Override [createStackElementTag] to include a append a "method name" to the tag.
+             */
+            override fun createStackElementTag(element: StackTraceElement): String {
+
+                return String.format(
+                    "%s:%s",
+                    element.methodName,
+                    super.createStackElementTag(element)
+                )
+            }
+        })
+        Timber.i("created")
     }
 }
