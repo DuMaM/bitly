a60f6a3c633b3f2013d04c0937ea6932ca7d0b9e
Bartosz Nowak <9051964+DuMaM@users.noreply.github.com>
2022-11-07 02:52:19 +0100

    feat: move to ecg view model

--- app/src/main/java/pl/nowak/bitly/ecg/EcgChartViewModel.kt
+++ app/src/main/java/pl/nowak/bitly/ecg/EcgChartViewModel.kt
@@ -1,33 +1,112 @@
 package pl.nowak.bitly.ecg
 
+import android.annotation.SuppressLint
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.*
+import pl.nowak.bitly.database.getDatabase
+import pl.nowak.bitly.repository.EcgDataRepository
 import timber.log.Timber
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
+@SuppressLint("MissingPermission")
 class EcgChartViewModel(application: Application) : AndroidViewModel(application) {
     var chartsDataList: MutableLiveData<List<EcgChartData>>
     var size = 12
+
+    private fun <T> MutableLiveData<T>.forceRefresh() {
+        this.postValue(value)
+    }
+
+    private val scope = CoroutineScope(Dispatchers.Main) // the scope of MyUIClass, uses Dispatchers.Main
+
+    @Volatile
+    private var isBlocked = false
+    private suspend fun triggerUpdateWithDelay(duration: Duration) {
+        if (isBlocked) {
+            return
+        }
+
+        isBlocked = true
+        scope.launch(Job()) {
+            delay(duration)
+            chartsDataList.forceRefresh()
+            Timber.d("Graph update triggered")
+            chartsDataList.value?.forEach {
+                Timber.d("${it.lineDataRestricted.toArray().contentToString()}")
+            }
+            isBlocked = false
+        }
+    }
+
+//    private var mutex = Mutex()
+//    private suspend fun triggerUpdateWithDelay(duration: Duration) {
+//        if (mutex.isLocked) {
+//            return
+//        }
+//
+//        mutex.withLock {
+//            scope.launch {
+//                delay(duration)
+//                chartsDataList.forceRefresh()
+//                Timber.d("Graph update triggered")
+//            }
+//        }
+//    }
+
 
     init {
         // remove error
         // Utils NOT INITIALIZED. You need to call Utils.init(...) at least once before calling Utils.convertDpToPixel(...). Otherwise conversion does not take place.
         chartsDataList = MutableLiveData(
-            listOf(
-                EcgChartData("Lead V1", 1, size),
-                EcgChartData("Lead V2", 2, size),
-                EcgChartData("Lead V3", 3, size),
-                EcgChartData("Lead V4", 4, size),
-                EcgChartData("Lead V5", 5, size),
-                EcgChartData("Lead V6", 6, size),
-                EcgChartData("Lead I", 7, size),
-                EcgChartData("Lead II", 8, size),
+            mutableListOf(
+                EcgChartData("Lead V1", 8, size),
+                EcgChartData("Lead V2", 4, size),
+                EcgChartData("Lead V3", 5, size),
+                EcgChartData("Lead V4", 6, size),
+                EcgChartData("Lead V5", 7, size),
+                EcgChartData("Lead V6", 1, size),
+                EcgChartData("Lead I", 2, size),
+                EcgChartData("Lead II", 3, size),
                 EcgChartData("Lead III", 9, size),
                 EcgChartData("Lead aVR", 10, size),
                 EcgChartData("Lead aVL", 11, size),
                 EcgChartData("Lead aVF", 12, size)
             )
         )
+
+        Timber.i("created")
+    }
+
+    private val database = getDatabase(application)
+    private val leadsRepository = EcgDataRepository(database, application)
+
+    init {
+        viewModelScope.launch {
+            while (true) {
+                leadsRepository.getData()?.collect {
+                    it.apply {
+                        val timestamp = System.currentTimeMillis().toFloat()
+                        chartsDataList.value?.get(0)?.update(timestamp, v1_c1.toFloat())
+                        chartsDataList.value?.get(1)?.update(timestamp, v2_c2.toFloat())
+                        chartsDataList.value?.get(2)?.update(timestamp, v3_c3.toFloat())
+                        chartsDataList.value?.get(3)?.update(timestamp, v4_c4.toFloat())
+                        chartsDataList.value?.get(4)?.update(timestamp, v5_c5.toFloat())
+                        chartsDataList.value?.get(5)?.update(timestamp, v6_c6.toFloat())
+                        chartsDataList.value?.get(6)?.update(timestamp, lead1.toFloat())
+                        chartsDataList.value?.get(7)?.update(timestamp, lead2.toFloat())
+                        chartsDataList.value?.get(8)?.update(timestamp, lead3.toFloat())
+                        chartsDataList.value?.get(9)?.update(timestamp, aVR.toFloat())
+                        chartsDataList.value?.get(10)?.update(timestamp, aVL.toFloat())
+                        chartsDataList.value?.get(11)?.update(timestamp, aVF.toFloat())
+                    }
+                    triggerUpdateWithDelay(1.seconds)
+                }
+            }
+        }
         Timber.i("created")
     }
 
@@ -37,3 +116,20 @@ class EcgChartViewModel(application: Application) : AndroidViewModel(application
     }
 }
 
+//                ecgData.apply {
+//                    val timestamp = System.currentTimeMillis().toFloat()
+//                    database.leadDao.insert(
+//                        LeadEntry(y = v1_c1.toFloat(), x = timestamp, lead = LeadName.LeadV1.ordinal),
+//                        LeadEntry(y = v2_c2.toFloat(), x = timestamp, lead = LeadName.LeadV2.ordinal),
+//                        LeadEntry(y = v3_c3.toFloat(), x = timestamp, lead = LeadName.LeadV3.ordinal),
+//                        LeadEntry(y = v4_c4.toFloat(), x = timestamp, lead = LeadName.LeadV4.ordinal),
+//                        LeadEntry(y = v5_c5.toFloat(), x = timestamp, lead = LeadName.LeadV5.ordinal),
+//                        LeadEntry(y = v6_c6.toFloat(), x = timestamp, lead = LeadName.LeadV6.ordinal),
+//                        LeadEntry(y = lead1.toFloat(), x = timestamp, lead = LeadName.LeadI.ordinal),
+//                        LeadEntry(y = lead2.toFloat(), x = timestamp, lead = LeadName.LeadII.ordinal),
+//                        LeadEntry(y = lead3.toFloat(), x = timestamp, lead = LeadName.LeadIII.ordinal),
+//                        LeadEntry(y = aVF.toFloat(), x = timestamp, lead = LeadName.LeadAVL.ordinal),
+//                        LeadEntry(y = aVR.toFloat(), x = timestamp, lead = LeadName.LeadAVR.ordinal),
+//                        LeadEntry(y = aVF.toFloat(), x = timestamp, lead = LeadName.LeadAVF.ordinal)
+//                    )
+//                }
