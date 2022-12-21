8fefe207b425e7564b2e087584893b73023fe3e4
Bartosz Nowak <9051964+DuMaM@users.noreply.github.com>
2022-12-21 01:03:06 +0100

    feat: add alternatives for android plot

---
+++ app/src/main/java/pl/nowak/bitly/ecg/EcgChartAdapter_test.kt
@@ -0,0 +1,66 @@
+package pl.nowak.bitly.ecg
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.androidplot.Plot
+import com.androidplot.util.Redrawer
+import com.androidplot.xy.BoundaryMode
+import com.androidplot.xy.LineAndPointFormatter
+import com.androidplot.xy.PanZoom
+import com.androidplot.xy.XYPlot
+import pl.nowak.bitly.databinding.EcgChartItemTestBinding
+
+
+class EcgChartListAdapterTest : ListAdapter<EcgChartData_Test, EcgChartListAdapterTest.EcgChartViewTestHolder>(EcgChartDiffCallback_Test()) {
+
+    override fun onBindViewHolder(holder: EcgChartViewTestHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EcgChartViewTestHolder {
+        return EcgChartViewTestHolder.from(parent)
+    }
+
+    class EcgChartViewTestHolder private constructor(binding: EcgChartItemTestBinding, val redrawer: Redrawer) : RecyclerView.ViewHolder(
+        binding
+            .root
+    ) {
+        private val ecgLabel: TextView = binding.ecgLineLabel
+        private val ecgChart: XYPlot = binding.ecgChart
+
+        fun bind(item: EcgChartData_Test) {
+            ecgLabel.text = item.label
+
+            if (ecgChart.registry.isEmpty) {
+                // add data series only once
+                val dataFormat = LineAndPointFormatter(Color.RED, Color.RED, null, null)
+                ecgChart.addSeries(item, dataFormat)
+            }
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): EcgChartViewTestHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = EcgChartItemTestBinding.inflate(layoutInflater, parent, false)
+
+                val plot: XYPlot = binding.ecgChart
+                plot.renderMode = Plot.RenderMode.USE_BACKGROUND_THREAD
+                plot.setRangeBoundaries(-200, 200, BoundaryMode.AUTO)
+                //plot.setDomainBoundaries(0, 2000, BoundaryMode.FIXED)
+                plot.legend.isVisible = false
+
+                // reduce the number of range labels
+                plot.linesPerRangeLabel = 3
+                val redrawer = Redrawer(plot, 10f, true)
+                PanZoom.attach(plot)
+
+                return EcgChartViewTestHolder(binding, redrawer)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/pl/nowak/bitly/ecg/EcgChartData_Test.kt
@@ -0,0 +1,146 @@
+package pl.nowak.bitly.ecg
+
+import android.graphics.Canvas
+import com.androidplot.Plot
+import com.androidplot.PlotListener
+import com.androidplot.SeriesRegistry
+import com.androidplot.ui.Formatter
+import com.androidplot.ui.SeriesBundle
+import com.androidplot.ui.SeriesRenderer
+import com.androidplot.xy.XYSeries
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import timber.log.Timber
+
+data class EcgChartData_Test(
+    val label: String,
+    val id: Int,
+    val size: Int
+) : XYSeries, PlotListener {
+
+    class SignalStats {
+        var n = 0
+        var K = 0f
+        var Ex = 0f
+        var Ex2 = 0f
+
+        fun add_variable(x: Float) {
+            if (n == 0)
+                K = x
+            n += 1
+            Ex += x - K
+            Ex2 += (x - K) * (x - K)
+        }
+
+        fun remove_variable(x: Float) {
+            n -= 1
+            Ex -= x - K
+            Ex2 -= (x - K) * (x - K)
+        }
+
+        fun get_mean(): Float {
+            return K + Ex / n
+        }
+
+        fun get_variance(): Float {
+            return (Ex2 - Ex * Ex / n) / (n - 1)
+        }
+
+        fun clean() {
+            K = 0f
+            Ex = 0f
+            Ex2 = 0f
+            n = 0
+        }
+    }
+
+    data class Entry(val x: Float, val y: Float)
+
+    var stats: SignalStats = SignalStats()
+
+    private var lineDataRestricted: ArrayDeque<Entry> = ArrayDeque()
+    private var seriesCounter = 0
+    private val seriesResolutionCounter = 16777215
+    private var cnt = 0
+    val mutex = Mutex()
+
+    @Volatile
+    var newVal: Boolean = false
+
+    fun clean() {
+        runBlocking {
+            mutex.withLock {
+                lineDataRestricted.clear()
+                seriesCounter = 0
+                stats.clean()
+                cnt = 0
+                Timber.i("Lead: $label was cleared")
+            }
+        }
+    }
+
+    suspend fun update(x: Float, y: Float) {
+        mutex.withLock {
+
+            if (cnt % 4 != 0) {
+                return
+            }
+
+            var x_sec = (x + seriesCounter * seriesResolutionCounter) / 1000000f
+            val y_scaled = y / (1000000f)
+            if (lineDataRestricted.size > 0 && x_sec < lineDataRestricted.last().x) {
+                seriesCounter++
+                x_sec = (x + seriesCounter * seriesResolutionCounter) / 1000000f
+            }
+            // add to the end
+            // time in ms, value in mv
+            val entry = Entry(x_sec, y_scaled)
+            if (lineDataRestricted.size > size) {
+                val removed = lineDataRestricted.removeFirst()
+                stats.remove_variable(removed.y)
+            }
+            lineDataRestricted.add(entry)
+            newVal = true
+            cnt++
+            stats.add_variable(y_scaled)
+
+            // Timber.i("For: ${label}:Variance is ${stats.get_mean() * 100000} ")
+        }
+    }
+
+
+    override fun getTitle(): String {
+        return label
+    }
+
+    override fun size(): Int {
+        return lineDataRestricted.size
+    }
+
+    override fun getX(p0: Int): Number {
+        return lineDataRestricted[p0].x
+    }
+
+    override fun getY(p0: Int): Number {
+        return lineDataRestricted[p0].y
+    }
+
+    override fun onBeforeDraw(
+        p0: Plot<*, out Formatter<*>, out SeriesRenderer<*, *, *>, out SeriesBundle<*, out Formatter<*>>, out SeriesRegistry<out SeriesBundle<*, out Formatter<*>>, *, out Formatter<*>>>?,
+        p1: Canvas?
+    ) {
+        runBlocking {
+            mutex.lock()
+        }
+    }
+
+    override fun onAfterDraw(
+        p0: Plot<*, out Formatter<*>, out SeriesRenderer<*, *, *>, out SeriesBundle<*, out Formatter<*>>, out SeriesRegistry<out SeriesBundle<*, out Formatter<*>>, *, out Formatter<*>>>?,
+        p1: Canvas?
+    ) {
+        runBlocking {
+            mutex.unlock()
+        }
+    }
+}
